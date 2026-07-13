7e9e9f8d86252bc86a7f8de54b986db91d20539f
grrr-h <hong303047@gmail.com>
2026-07-14 01:10:08 +0900

    feat : StoreApplicationStatus/StoreOperationStatus BaseEnum으로 구현 #89

--- src/main/java/com/sparta/spartachallenge8282/store/domain/StoreApplicationStatus.java
+++ src/main/java/com/sparta/spartachallenge8282/store/domain/StoreApplicationStatus.java
@@ -1,7 +1,17 @@
 package com.sparta.spartachallenge8282.store.domain;
 
-public enum StoreApplicationStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
+import com.sparta.spartachallenge8282.global.common.BaseEnum;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum StoreApplicationStatus implements BaseEnum {
+
+    PENDING("PENDING", "등록 신청"),
+    APPROVED("APPROVED", "승인"),
+    REJECTED("REJECTED", "거절");
+
+    private final String code;
+    private final String description;
 }
